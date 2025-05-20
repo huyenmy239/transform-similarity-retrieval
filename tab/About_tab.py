4a8208e0bffa9db91971c2f9ae84458495776961
huyenmy239 <nguyenthihuyenmy239@gmail.com>
2025-05-20 15:45:42 +0700

    Edit About tab

--- tab/About_tab.py
+++ tab/About_tab.py
@@ -11,5 +11,5 @@ class TabAbout:
         frame.pack(padx=20, pady=20, fill="both", expand=True)
 
         tk.Label(frame, text="About Object Image Editor", font=("Helvetica", 12, "bold"), bg="#f0f4f8", fg="#2c3e50").pack(pady=10)
-        tk.Label(frame, text="Phiên bản: 1.0\nTác giả: Nhóm 06\nMô tả: Ứng dụng chỉnh sửa ảnh với các phép biến đổi.", 
+        tk.Label(frame, text="Phiên bản: 1.0\nTác giả: Nhóm 02\nMô tả: Ứng dụng chỉnh sửa ảnh với các phép biến đổi.", 
                  font=("Helvetica", 10), bg="#f0f4f8", justify="left").pack(pady=5)
